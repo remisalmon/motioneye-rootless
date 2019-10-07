a2511bfedcdc0aa9824c002313540ac6990b2fc7
Remi Salmon <salmon.remi@gmail.com>
2019-10-07 16:26:36 -0600

    Fix image tag

--- start_motioneye_docker.sh
+++ start_motioneye_docker.sh
@@ -2,7 +2,7 @@
 
 MOTIONEYE_FOLDER=$(pwd)/docker
 
-docker run --detach --publish 8765:8765 --hostname motionEye --volume $MOTIONEYE_FOLDER:/motioneye --device /dev/video4:/dev/video0 localhost/motioneye:bionic
+docker run --detach --publish 8765:8765 --hostname motionEye --volume $MOTIONEYE_FOLDER:/motioneye --device /dev/video4:/dev/video0 localhost/motioneye:latest
 #docker run --detach --publish 8765:8765 --hostname motionEye --volume $MOTIONEYE_FOLDER:/motioneye:Z --device /dev/video4:/dev/video0 --security-opt label=disable localhost/motioneye:latest
 
 xdg-open 'http://localhost:8765'
