8ea04382271ea20c35132770e550e0df149fe19e
Remi Salmon <salmon.remi@gmail.com>
2019-10-08 15:50:59 -0600

    Bug fix

--- install_motioneye.sh
+++ install_motioneye.sh
@@ -18,6 +18,6 @@ export PYCURL_SSL_LIBRARY=openssl
 
 pip install --no-cache-dir pycurl
 
-mkdir -p MOTIONEYE_FOLDER
+mkdir -p $MOTIONEYE_FOLDER
 
 cat $VENV_FOLDER/share/motioneye/extra/motioneye.conf.sample | sed -e "s?/etc/motioneye?$MOTIONEYE_FOLDER?" -e "s?/var/run?$MOTIONEYE_FOLDER?" -e "s?/var/log?$MOTIONEYE_FOLDER?" -e "s?/var/lib/motioneye?$MOTIONEYE_FOLDER?" > $MOTIONEYE_FOLDER/motioneye.conf
