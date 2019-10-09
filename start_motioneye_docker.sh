b0c23fcc35e1b761bd3a6fc5ca7430c3996d7087
Remi Salmon <salmon.remi@gmail.com>
2019-10-09 10:39:04 -0600

    Change default to /dev/video0, don't open browser

--- start_motioneye_docker.sh
+++ start_motioneye_docker.sh
@@ -2,6 +2,4 @@
 
 MOTIONEYE_FOLDER=$(pwd)/motioneye
 
-docker run --detach --publish 8765:8765 --hostname motionEye --volume /etc/localtime:/etc/localtime:ro --volume $MOTIONEYE_FOLDER:/motioneye --device /dev/video4:/dev/video0 localhost/motioneye:latest
-
-xdg-open 'http://localhost:8765'
+docker run --detach --publish 8765:8765 --hostname motionEye --volume /etc/localtime:/etc/localtime:ro --volume $MOTIONEYE_FOLDER:/motioneye --device /dev/video0:/dev/video0 localhost/motioneye:latest
