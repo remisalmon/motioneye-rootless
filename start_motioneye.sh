c0958e6a5a5669b98cb64ba14238221353cfc64f
Remi Salmon <salmon.remi@gmail.com>
2019-10-13 19:12:39 -0600

    Simplify

--- start_motioneye.sh
+++ start_motioneye.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VENV_FOLDER=$(pwd)/venv27
+VENV_FOLDER=venv27
 
-MOTIONEYE_FOLDER=$(pwd)/motioneye
+MOTIONEYE_FOLDER=motioneye
 
 source $VENV_FOLDER/bin/activate && meyectl startserver -c $MOTIONEYE_FOLDER/motioneye.conf 2> $MOTIONEYE_FOLDER/motioneye.log &
 
